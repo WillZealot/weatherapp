ac07c5cdee0af6a23023ec344366164213bd9898
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-05-19 20:39:13 -0400

    added a search bar and styles to it

--- develop/dostuff.js
+++ develop/dostuff.js
@@ -1,5 +1,4 @@
 let APIKey = "da1e61f41c58073ff7267566948f1acc";
+let apiUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}" + "&appid=" + APIKey;
 
-let cityWeather = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-fetch(queryURL)
+fetch(apiUrl)
